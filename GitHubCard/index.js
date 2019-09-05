a58bc0abc4ed7bdfe9ab0d5c6d9071907411a858
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-05 11:43:44 +0000

    Added function to fetch github user data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,20 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const getGithubUserData = () => {
+  let userData;
+  axios
+    .get("https://api.github.com/users/aapetsi")
+    .then(res => (userData = res.data))
+    .catch(error => {
+      console.log(error);
+    });
+  return userData;
+};
 
+const githubUserData = getGithubUserData();
+
+console.log(githubUserData.data);
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -29,9 +42,66 @@ const followersArray = [];
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
+*/
+const githubCardCreator = userInfo => {
+  // select parent div
+  const parentDiv = document.querySelector(".cards");
+  // create dom elements
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
+
+  const userImage = document.createElement("img");
+  userImage.src = "";
+
+  const userProfileLink = document.createElement("a");
+  userProfileLink.href = "";
+
+  const divCardInfo = document.createElement("div");
+  divCardInfo.classList.add("card-info");
+
+  const h3 = document.createElement("h3");
+  h3.classList.add("name");
+
+  // create 7 paragraph tags
+  const paragraphList = [];
+  for (let i = 0; i <= 6; i++) {
+    paragraphList.push(document.createElement("p"));
+  }
+
+  const [
+    userName,
+    location,
+    profile,
+    followers,
+    following,
+    bio
+  ] = paragraphList;
+
+  userName.classList.add("username");
+
+  // append created elements to parent div
+  parentDiv.appendChild(divCard);
+  divCard.appendChild(userImage);
+  divCard.appendChild(divCardInfo);
+  divCardInfo.appendChild(h3);
+  divCardInfo.appendChild(userName);
+  divCardInfo.appendChild(location);
+  divCardInfo.appendChild(profile);
+  profile.appendChild(userProfileLink);
+  divCardInfo.appendChild(followers);
+  divCardInfo.appendChild(following);
+  divCardInfo.appendChild(bio);
+
+  return divCard;
+};
+
+githubCardCreator();
+
+/*
+
+<div class="card"> 
+  <img src={image url of user} /> 
+  <div class="card-info"> 
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
